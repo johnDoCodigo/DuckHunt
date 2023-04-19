6674823d0b41888e28fa73d6bcd812f054e4ece6
tiagosilva28 <tiagosilva28@live.com.pt>
2023-04-19 14:37:03 +0100

    aim and grass

--- scripts/duck.js
+++ scripts/duck.js
@@ -15,3 +15,24 @@ else
 }
 , interval ); //end of setInterval
 } //end of animateScript()
+
+
+var tID2; //we will use this variable to clear the setInterval()
+function animateScript2() {
+var    position = 70; //start position for the image slicer
+const  interval = 100; //100 ms of interval for the setInterval()
+tID2 = setInterval ( () => {
+document.getElementById("duck").style.backgroundPosition = 
+`-${position}px 0px`; 
+//we use the ES6 template literal to insert the variable "position"
+if (position < 210)
+{ position = position + 70;}
+//we increment the position by 256 each time
+else
+{ position = 70; }
+//reset the position to 256px, once position exceeds 1536px
+}
+, interval ); //end of setInterval
+}
+
+
