f2cd6ced1e86c16c8bcdd318bb2163a5fb69a8d1
tiagosilva28 <tiagosilva28@live.com.pt>
2023-04-22 13:01:38 +0100

    aa

--- scripts/duck.js
+++ scripts/duck.js
@@ -19,6 +19,7 @@ window.onload = () => {
     nextDuckId++;
     ducks.push(duck);
     duckIds.push(duck.idName);
+
     return duck;
   };
 
@@ -36,29 +37,28 @@ window.onload = () => {
   function animateDuckRandom(duck) {
     const duckId = duck.idName;
     let element = document.getElementById(duckId);
-  
+
     const directions = ["right", "left", "right-top", "left-top"];
-    const randomDirection = directions[Math.floor(Math.random() * directions.length)];
-  
+    const randomDirection =
+      directions[Math.floor(Math.random() * directions.length)];
+
     element.classList.toggle("duck-" + randomDirection);
-  
 
     tID = setInterval(() => {
       if (element) {
         element.style.backgroundPosition = `-${position}px 0px`;
       }
-  
+
       if (position < 210) {
         position = position + 70;
       } else {
         position = 70;
-
       }
     }, interval);
 
-
     element.addEventListener("animationend", function handleAnimationEnd() {
-      const newRandomDirection = directions[Math.floor(Math.random() * directions.length)];
+      const newRandomDirection =
+        directions[Math.floor(Math.random() * directions.length)];
       element.classList.toggle("duck-" + randomDirection);
       element.classList.toggle("duck-" + newRandomDirection);
       console.log(randomDirection);
@@ -66,28 +66,23 @@ window.onload = () => {
       element.removeEventListener("animationend", handleAnimationEnd);
       element.addEventListener("animationend", handleAnimationEnd);
     });
-  
   }
-  
-  const switchDirections = (duck) => {
 
+  const switchDirections = (duck) => {
     tID = setInterval(() => {
       if (element) {
         element.style.backgroundPosition = `-${position}px 0px`;
       }
-  
+
       if (position < 210) {
         position = position + 70;
       } else {
         position = 70;
-
       }
     }, interval);
 
-    console.log("here i am")
-
     root = document.documentElement;
-  
+
     let positionX = duck.positionX;
     let positionY = duck.positionY;
 
@@ -96,98 +91,99 @@ window.onload = () => {
 
     element.classList.remove("duckAnimation");
 
-    let newPositionX = getRandomInt(25,95);
-    let newPositionY = getRandomInt(25,95);
-    
+    let newPositionX = getRandomInt(25, 95);
+    let newPositionY = getRandomInt(25, 95);
+    console.log(duck.idName, newPositionX, newPositionY)
+
     if (newPositionX > positionX && newPositionY != positionY) {
-      
       element.classList.add("duck-right-top");
-      
-    }
-
-    else if (newPositionX < positionX && newPositionY != positionY){
-      
+    } else if (newPositionX < positionX && newPositionY != positionY) {
       element.classList.add("duck-left-top");
-    }
-    else if(newPositionX == positionX && newPositionY > positionY){
-      
+    } else if (newPositionX == positionX && newPositionY > positionY) {
       element.classList.add("right");
-    }
-
-    else if(newPositionX == positionX && newPositionY < positionY){
-      
+    } else if (newPositionX == positionX && newPositionY < positionY) {
       element.classList.add("left");
     }
-/*     else {
+    /*     else {
       newPositionX = getRandomInt(25,100);
       newPositionY = getRandomInt(25,100);
     } */
 
-    root.style.setProperty('--inicialX', positionX + "%");
-    root.style.setProperty('--inicialY', positionY + "%");
+    root.style.setProperty("--inicialX", positionX + "%");
+    root.style.setProperty("--inicialY", positionY + "%");
 
-    root.style.setProperty('--finalX', newPositionX + "%");
-    root.style.setProperty('--finalY', newPositionY + "%");
+    root.style.setProperty("--finalX", newPositionX + "%");
+    root.style.setProperty("--finalY", newPositionY + "%");
 
     duck.positionX = newPositionX;
     duck.positionY = newPositionY;
 
     void element.offsetWidth; // restart da ANIMATION!!!!
     element.classList.add("duckAnimation");
-
   };
 
-
-
-
-  const animateDeadDuck = (duck, positionX, positionY) => {
+  const animateDeadDuck = (duckElement, positionX, positionY) => {
     clearInterval(tID);
-    duck.className = "";
-    duck.style = "";
-    duck.classList.toggle("duck-dead");
+    duckElement.className = "";
+    duckElement.style = "";
+    duckElement.classList.toggle("duck-dead");
 
-    duck.style.top = `${positionY}px`;
-    duck.style.left = `${positionX}px`;
+
+    tID = setInterval(() => {
+      if (duckElement) {
+        duckElement.style.backgroundPosition = `-${position}px 0px`;
+      }
+
+      if (position < 210) {
+        position = position + 70;
+      } else {
+        position = 70;
+      }
+    }, interval);
+
+    duckElement.style.top = `${positionY}px`;
+    duckElement.style.left = `${positionX}px`;
   };
 
-    //tests of create and render ducks
-    let duck1 = createDuck(35, 5);
-    generateDuck(duck1);
-    switchDirections(duck1);
+  //tests of create and render ducks
+  let duck1 = createDuck(35, 5);
+  //let duck2 = createDuck(20, 10);
+  generateDuck(duck1);
+  //generateDuck(duck2);
 
+  switchDirections(duck1);
+  //switchDirections(duck2);
 
   //kill duck
-  duckIds.forEach(function(duckId) {
-    const duckeElement = document.getElementById(duckId);
+  ducks.forEach(function (duck) {
+    const duckeElement = document.getElementById(duck.idName);
+
     duckeElement.addEventListener("click", function () {
       duckeElement.removeEventListener("animationend", arguments.callee);
-  
+
       const rect = duckeElement.getBoundingClientRect();
       const x = rect.left + rect.width / 2;
       const y = rect.top + rect.height / 2;
-  
+
       // Get the position of the duck
       console.log(`You clicked the duck at position (${x}, ${y})`);
 
-      duckeElement.classList.toggle("dead-duck");
+      //duckeElement.classList.toggle("dead-duck");
 
-  
       animateDeadDuck(duckeElement, x, y);
     });
 
-    duckeElement.addEventListener("animationend", function() {
-      console.log("WOOW")
+    duckeElement.addEventListener("animationend", function () {
       clearInterval(tID);
-      switchDirections(duck1);
+      switchDirections(duck);
+      //switchDirections(duck2);
+
     });
   });
-
 
   // utilities functions
 
   function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-
-
 };
